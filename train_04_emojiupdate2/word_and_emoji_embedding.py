e515ee380900804c28187e07e088cd2cee84d48e
changsiteng <cstvita@163.com>
2019-10-14 21:46:26 +0800

    update

--- train_04_emojiupdate2/word_and_emoji_embedding.py
+++ train_04_emojiupdate2/word_and_emoji_embedding.py
@@ -55,7 +55,7 @@ class Tensor:
     test_data = ''
     valid_data = ''
     batch_size = ''
-    path = '../train_04_emojiupdate2/data/'
+    path = '../train_04_emojiupdate2/data_emoji_and_split/'
     train_json_name = 'train_data.json'
     test_json_name = 'test_data.json'
 
