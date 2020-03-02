aa3903bda11bae81abba333f400e0ad37eb8bd9d
changsiteng <cstvita@163.com>
2020-03-02 11:00:51 +0800

    update

--- train_04_emojiupdate2/train.py
+++ train_04_emojiupdate2/train.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
     '''
           命令行参数传递类型
           '''
-    dataFolder, model_path, lossType = getType()
+    dataFolder, model_path, lossType, isCall, topic = getType()
 
     BATCH_SIZE = 64
     SEED = 1234
