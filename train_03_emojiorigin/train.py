26791ba05816e09e84c770225f997809ba4df8cd
changsiteng <cstvita@163.com>
2020-03-02 10:42:42 +0800

    update

--- train_03_emojiorigin/train.py
+++ train_03_emojiorigin/train.py
@@ -155,7 +155,7 @@ if __name__ == '__main__':
     '''
           命令行参数传递类型
           '''
-    dataFolder, model_path, lossType,isCall = getType()
+    dataFolder, model_path, lossType, isCall, topic = getType()
 
     BATCH_SIZE = 64
     SEED = 1234
