2f7d8a1812455097490c72475925144ac2486b3f
changsiteng <cstvita@163.com>
2020-03-09 16:59:15 +0800

    取消打印

--- train_04_emojiupdate3_updateAttention/lstm_emoji_attention.py
+++ train_04_emojiupdate3_updateAttention/lstm_emoji_attention.py
@@ -172,7 +172,6 @@ class EMOJI_ATTENTION_LSTM(nn.Module):
             '''
 
             # 所有分句的Attention输出 整合在一起 all_out[sentence_num,batch_size,hidden_size]
-            print(attention_out.size())
             if len(all_out) == 0:
                 all_out = attention_out
             else:
